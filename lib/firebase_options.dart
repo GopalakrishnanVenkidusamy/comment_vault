36bfb6f2315fec1d3b0b140f25ab337301adff08
Gopalakrishnan <gopalakrishnanvenkidusamy@gmail.com>
2024-10-05 10:12:03 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,16 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, TargetPlatform;
 
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
 class DefaultFirebaseOptions {
 
   static const FirebaseOptions android =  FirebaseOptions(
@@ -38,16 +28,5 @@ class DefaultFirebaseOptions {
         );
     }
   }
-
-// static const FirebaseOptions ios = FirebaseOptions(
-//   apiKey: 'AIzaSyCCM7HloaBR-ssXimYx9m3NmxeKMJad0CQ',
-//   appId: '1:389273765439:ios:65d19d07702512e3c3da8a',
-//   messagingSenderId: '383294896292',
-//   projectId: 'com.bala.tracker',
-//   storageBucket: 'bala-expense-tracker.appspot.com',
-//   iosClientId: '335311491157-c5flgeet9q75j3nrujvpjp21hljn7f5c.apps.googleusercontent.com',
-//   iosBundleId: 'com.bala.tracker',
-// );
-
 }
 
